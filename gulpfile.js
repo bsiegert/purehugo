865e38961ce50de8b0ef0a4614362feb489a35fa
Benny Siegert <bsiegert@gmail.com>
2020-12-17 14:22:20 +0100

    Get rid of Rainbow in favor of Chroma.
    Recent Hugo versions come with Chroma for syntax highlighting,
    which clashes with the Rainbow module included via JS.
    
    Fix styles a bit to have consistent foreground and background
    in the output.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
 
 function compress_js(cb) {
-  src(['assets/js/jquery.min.js', 'assets/js/jquery.prettysocial.min.js', 'assets/js/rainbow-custom.min.js', 'assets/js/scripts.js'])
+  src(['assets/js/jquery.min.js', 'assets/js/jquery.prettysocial.min.js', 'assets/js/scripts.js'])
     .pipe(concat('all.min.js'))
     .pipe(uglify())
     .pipe(dest('static/js/'));
@@ -12,7 +12,7 @@ function compress_js(cb) {
 }
 
 function compress_css(cb) {
-  src(['assets/css/blog.css', 'assets/css/syntax-highlighter.css'/*, 'assets/css/custom.css' */])
+  src(['assets/css/blog.css', 'assets/css/syntax-highlighter.css', /*'assets/css/custom.css' */])
     .pipe(concat('all.min.css'))
     .pipe(minifyCSS())
     .pipe(dest('static/css/'));
